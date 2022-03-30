1deb3c9f403998a767e7f438d857ea23e0dd52c5
John Netzel <johnrnetzel@gmail.com>
2022-03-30 11:12:03 -0800

    Add winner evaluation

--- javascript.js
+++ javascript.js
@@ -6,6 +6,19 @@ if(validateUserInput())
     alert("You chose " + playerMove);
     computerMove = getComputerMove();
     alert("The computer chose " + computerMove)
+    var result = getWinner();
+    if (result === "Tie")
+    {
+        alert("You tied.");
+    }
+    else if (result === "Win")
+    {
+        alert("You Win!");
+    }
+    else if (result === "Loss")
+    {
+        alert("You lost!");
+    }
 }
 
 function validateUserInput()
@@ -20,4 +33,39 @@ function getComputerMove()
     move = move * possibleMoves.length; // 0 to 3, floating point
     move = Math.floor(move); // 0, 1, or 2
     return possibleMoves[move]; // Reaches inside array to grab random value
+}
+
+function getWinner()
+{
+    if (playerMove === computerMove)
+    {
+        return "Tie";
+    }
+    // Player win conditions
+    else if (playerMove === "P" && computerMove === "R")
+    {
+        return "Win";
+    }
+    else if (playerMove === "S" && computerMove === "P")
+    {
+        return "Win";
+    }
+    else if (playerMove === "R" && computerMove === "S")
+    {
+        return "Win";
+    }
+
+    // Player loss conditions
+    else if (playerMove === "P" && computerMove === "S")
+    {
+        return "Loss";
+    }
+    else if (playerMove === "S" && computerMove === "R")
+    {
+        return "Loss";
+    }
+    else if (playerMove === "R" && computerMove === "P")
+    {
+        return "Loss";
+    }
 }
