535f5296057538941fa830211e47d8e4d02a6d2e
John Netzel <johnrnetzel@gmail.com>
2022-03-30 10:52:57 -0800

    Add user input and validation

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-alert("Alert");
+var playerMove = prompt("Enter a letter: R, P, or S");
+playerMove = playerMove.toUpperCase(); // Makes comparisons easier
+if(validateUserInput())
+{
+    alert("Input is valid.");
+}
+
+function validateUserInput()
+{
+    return playerMove === "R" || playerMove === "P" || playerMove === "S";
+}
